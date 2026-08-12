04616305686ec292039f7acdaa0bc45afa9f6831
William Zhang <william.zhang.wzz2@yale.edu>
2026-08-12 12:08:35 -0400

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        min_price = 101
+        
+        for price in prices:
+            
+            if price - min_price > max_profit:
+                max_profit = price - min_price
+
+            if price < min_price:
+                min_price = price
+        return max_profit
