f8a9e98af81d2d2d5fd7f887cb206ebc1ad0f232
William Zhang <william.zhang.wzz2@yale.edu>
2026-08-27 14:14:37 -0400

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.py
@@ -0,0 +1,24 @@
+from collections import deque
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        queue = deque()
+        r = len(grid)
+        c = len(grid[0])
+
+        for i in range(r):
+            for j in range(c):
+                if grid[i][j] == 0:
+                    queue.append((i,j))
+
+        while queue:
+            i, j = queue.popleft()
+
+            sides = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+            for di, dj in sides:
+                if 0 <= di + i < r and 0 <= dj + j < c and grid[di+i][dj+j] == 2147483647:
+                    grid[di+i][dj+j] = grid[i][j] + 1
+
+                    queue.append((di+i, dj+j))
+                    
+        
